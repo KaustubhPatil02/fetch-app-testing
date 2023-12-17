75b770da12f999d6118b9aef006deaf74eadc1f1
Patil Kaustubh <patilkaustubh990@gmail.com>
2023-12-17 23:09:16 +0530

    git_push

---
+++ src/firebaseconfig.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCaftPr1tooRnX_i5MTeL1b4efwzZrF4po",
+  authDomain: "fetch-6cd11.firebaseapp.com",
+  projectId: "fetch-6cd11",
+  storageBucket: "fetch-6cd11.appspot.com",
+  messagingSenderId: "865727908379",
+  appId: "1:865727908379:web:cd491ff0f1cd80495a86be"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
